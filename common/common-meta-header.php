2bad0b8fc94248550b5ab81a20fea72d0436ffbf
thuyn789 <thuynh0135@gmail.com>
2021-04-30 01:41:18 -0400

    Updated index page and db.php

--- common/common-meta-header.php
+++ common/common-meta-header.php
@@ -1,6 +1,6 @@
 <!-- 
-Project 2 - Team Mysterious - Web Programming
-Project name: The Game of Life
+Project 4 - Final - Team Mysterious - Web Programming
+Project name: Super Cool Rental - Parking Services
 Team members: Tin Huynh, Hyunki Lee, JiaWen Li, Sang Kim, Isaiah Smith
 
 Version: 1.0 beta
@@ -11,7 +11,7 @@ Version: 1.0 beta
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="style.css" type="text/css" />
-	<link rel="icon" type="imag/gif" href="./images/gameoflife.png"/>
-	<title>The Game of Life</title>
+	<link rel="icon" type="imag/gif" href="./images/web_icon.png"/>
+	<title>Super Cool Rental</title>
 </head>
 <body>
--- index.php
+++ index.php
@@ -1,99 +1,35 @@
-<?php 
-    if(isset($_POST['Submit'])) {
-
-        #$logins = array('Test' => '123456');
-        $logins = array(); #holds login info
-
-        $acc_file =  fopen("user_database/user_account.txt","r"); #open file containing account information
-
-        /*Read through file line by line and build associative array with the contents */
-        if($acc_file) { 
-            while (($line = fgets($acc_file))  !== false) {
-
-                $info = explode(',', $line); #Separate each line
-                /*
-                $info[0] = username
-                $info[1] = email
-                $info[2] = password
-                */ 
-                $logins[$info[0]] = $info[2]; #add credentials to associative array
-            }
-        }
-
-        /* Check and assign submitted Username and Password to new variable */
-        $Username = isset($_POST['username']) ? $_POST['username'] : '';
-        $Password = isset($_POST['password']) ? $_POST['password'] : '';
-        
-
-    #Check if credentials are in logins array
-    if(array_key_exists($Username, $logins) && trim($logins[$Username]) == trim($Password)) { #Credentials are found
-        
-        //header("location:level_difficulty.php"); #goto index.php
-		header("location:game.html"); #goto game.html
-        exit(); #end php script
-    } else {
-        print("credentials not found in array<br>");
-    }
-}
-?>
-
 <?php include 'common/common-meta-header.php'; ?>
 
-<!--
-<div class="icon">
-    <img src="images/LogIn.JPG">
+<div class="container">
+	<div class="navibar">
+		<img src="images/brand_logo.png" class="brand_logo" alt="logo image">
+		<nav>
+			<ul>
+				<li><a href="index.php">HOME</a></li>
+				<li><a href="login.php">LOGIN/SIGNUP</a></li>
+				<li><a href="#">OUR TEAM</a></li>
+				<li><a href="#">CREDITS</a></li>
+			</ul>
+		</nav>
+	</div>
+
+	<div class="welcome">
+		<div class="intro">
+			<h1>Welcome to Super Cool Rental</h1>
+			<p>Where we offer super affordable rate for car rental and parking services</p>
+		</div>
+		<div class="services">
+			<div class="pic">
+				<h3>Car Rental</h3>
+				<img src="images/rental.jpg" class="rental_image" alt="rental image">
+			</div>
+			<div class="pic">
+				<h3>Parking Services</h3>
+				<img src="images/parking.jpg" class="parking_image" alt="parking image">
+			</div>
+		</div>
+	</div>
 </div>
--->
-<div>
-    <img class="title_img" src="./images/Title.JPG" alt="image">
-</div>
-
-    <form class="input" method="POST" name="login_form" id="input_form">
-        <fieldset>
-            <legend class="legend_txt">Login</legend>
-            <table> <!-- This table will contain the fields for the form-->
-
-                <!--Username label & textfield row-->
-                <tr> 
-                    <td>
-                        <label>Username:</label>
-                    </td>
-                    <td>
-                        <input name="username" type="text" size="16" placeholder="Username">
-                    </td>
-                </tr>
-
-                <!--Password label & textfield row-->
-                <tr>
-                    <td>
-                        <label>Password:</label>
-                    </td>
-
-                    <td>
-                        <input type="password" name="password" size="16" placeholder="Password">
-                    </td>
-
-                </tr>
-
-
-                <!--Submit button row-->
-                <tr class="centered">
-                    <td colspan="2" class="centered">
-                        <input name="Submit" type="submit" value="Login" id="submit-btn">
-                    </td>
-
-                </tr>
-
-                <!--Goto signup page row-->
-                <tr>
-                    <td colspan="2" class="centered">
-                        <a href="./signup.php"> Don't have an account? Sign up!</a>
-                    </td>
-
-                </tr>
-            </table>
-        </fieldset>
-    </form>
 
 <!-- shared page bottom HTML -->
 <?php include 'common/common-footer.php'; ?>
